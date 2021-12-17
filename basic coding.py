8ea8a5626d6eb5bcc9fc59e9e7892089ad2142f0
iamala <96304056+iamala@users.noreply.github.com>
2021-12-17 22:40:08 +0700

    Add files via upload

---
+++ basic coding.py
@@ -0,0 +1,10 @@
+num = "5"
+num2 = float("2.7")
+num3 = int("1")
+name = "basic coding."
+print(num[0])
+print(num2//num3)
+print("the file name is" ,name," ")
+print("i can code! but this is just some" ,name," ")
+print("And this is math.")
+print(7 + 232 // 6)
